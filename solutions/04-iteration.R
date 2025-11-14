617b0c774119ff7befc69b6b533ef6b6094b41cf
Andy Teucher <andy.teucher@gmail.com>
2025-11-13 18:40:05 -0800

    don't need pkg name

--- solutions/04-iteration.R
+++ solutions/04-iteration.R
@@ -3,10 +3,10 @@ library("readxl")
 library("fs")
 
 ## Our turn
-data1952 <- readxl::read_excel("data/gapminder/1952.xlsx")
-data1957 <- readxl::read_excel("data/gapminder/1957.xlsx")
-data1962 <- readxl::read_excel("data/gapminder/1962.xlsx")
-data1967 <- readxl::read_excel("data/gapminder/1967.xlsx")
+data1952 <- read_excel("data/gapminder/1952.xlsx")
+data1957 <- read_excel("data/gapminder/1957.xlsx")
+data1962 <- read_excel("data/gapminder/1962.xlsx")
+data1967 <- read_excel("data/gapminder/1967.xlsx")
 
 data_manual <- bind_rows(data1952, data1957, data1962, data1967)
 
