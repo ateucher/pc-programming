9dd37aed6a64dc3f5fbf9c7af1441a75fa71090e
Andy Teucher <andy.teucher@gmail.com>
2025-11-14 17:20:24 -0800

    Add parallel

--- solutions/05-iteration-2.R
+++ solutions/05-iteration-2.R
@@ -25,9 +25,10 @@ run_pa <- function(
   fxsize,
   yearcol = "nyear",
   extend_n = 10,
-  nsim = 100,
+  nsim = 500,
   alpha = 0.2
 ) {
+  library(simr)
   fixef(model)[yearcol] <- (log(fxsize))
   model_extended <- extend(model, along = yearcol, n = extend_n)
   result <- model_extended |>
@@ -38,7 +39,7 @@ run_pa <- function(
     summary() |>
     as.data.frame()
 
-  mutate(result, fxsize = fxsize, Design = "Full dataset")
+  dplyr::mutate(result, fxsize = fxsize, Design = "Full dataset")
 }
 
 fx_sizes <- seq(1.01, 1.05, by = 0.01)
@@ -50,3 +51,39 @@ res_list <- map(
 
 power_results <- list_rbind(res_list)
 power_results
+
+##########
+
+# We can do this in parallel to make it faster. Let's try with 100 simulations
+# with 10 effect sizes
+
+fx_sizes <- seq(1.01, 1.10, by = 0.01)
+
+tictoc::tic("Sequential")
+res_list <- map(
+  fx_sizes,
+  \(x) run_pa(rodent_mod, x, nsim = 100)
+)
+tictoc::toc()
+
+# This is weird, we have to add the data back into the model object so that
+# when it gets sent into the parallel workers it has everything it needs
+getData(rodent_mod) <- rodent_counts
+
+parallel::detectCores()
+
+tictoc::tic("Parallel, 10 workers")
+mirai::daemons(10)
+
+res_list <- map(
+  fx_sizes,
+  in_parallel(
+    \(x) run_pa(model, x, nsim = 100),
+    # need to send the objects into the workers
+    run_pa = run_pa,
+    model = rodent_mod
+  )
+)
+
+mirai::daemons(0)
+tictoc::toc()
